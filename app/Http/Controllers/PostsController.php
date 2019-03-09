e380382ffc40f2a60706cb6fcfa0bd6fa3c854e7
“朱聪” <tniub.cc@gmail.com>
2019-03-09 03:40:25 +0000

    使用map 重新拼装数据

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $users = User::query()->limit(4)->get();
+
+        $users = $users->map(function ($user) {
+            //可以考虑$user->id缓存,在保证了速度的同时避免大面积的缓存重建
+            $user->posts = $user->posts()->limit(3)->get();
+
+            return $user;
+        });
+
+        dd($users);
+    }
+}
